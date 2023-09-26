a0b21d2f140d307f69f0a7bfc93969939ce055c9
Carter Slade <CarterSlade7@protonmail.com>
2023-09-26 09:46:27 -0700

    Better comments/documentation
    It is a good habit to make proper comments, but I also did a tiny change in the code.
    
    Thanks for reading, and I hope you have a grand day!

--- main.py
+++ main.py
@@ -2,35 +2,45 @@ import discord, os
 from discord import app_commands
 from discord.ext import commands
 
+## It's a healthy habit not to have one's API keys visible on github
 file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'chuckie_cheese.token')
 
 with open(file_path, 'r') as file:
     chuckie_cheese = file.readline().strip()
 
+## my intentions are pure
 intents = discord.Intents.all()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=">", intents=intents)
+## The bot/client itself
+client = commands.Bot(command_prefix=">", intents=intents)
 
-@bot.event
+## --> events
+@client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(bot))
+    print('Logged in as {0.user}'.format(client))
     try:
-        synced = await bot.tree.sync()
-        print(f'The commands {len(synced)} are synced')
+        synced = await client.tree.sync()
+        print(f'Commands synced: {len(synced)}')
     except Exception as e:
         print(f'Ran into error: {e}')
     print('This is Gold Leader, standing by!')
 
-@bot.event
+## when someone sends a message, see which of these applies
+@client.event
 async def on_message(message):
-    if message.content == "F" or message.content == "f":
+    ## if someone writes f in chat, react with f in chat
+    if (message.content).lower() == "f":
         await message.add_reaction("🇫")
 
-@bot.tree.command(name="test")
+
+## --> commands
+
+## test command
+@client.tree.command(name="test")
 async def test(interaction: discord.Interaction):
     await interaction.response.send_message(f'{interaction.user.mention} has summoned the angry one')
 
 
-
-bot.run(chuckie_cheese)
+## actually run the bot
+client.run(chuckie_cheese)
